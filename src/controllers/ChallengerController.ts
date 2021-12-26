b503267eb7d78e9717a1ffb9fa722bc67fe347f4
BlueJayGM <damato578@gmail.com>
2021-12-26 17:59:57 -0300

    controller and provider added

--- src/controllers/ChallengerController.ts
+++ src/controllers/ChallengerController.ts
@@ -14,25 +14,101 @@ router.post('/', async (req, res) => {
 
 	if ( challenger.validate() ) {
 		try {
-			await provider.create(challenger);
-			res.statusCode = 200;
-			res.json('Challenger criado com sucesso!');
+			const result = await provider.create(challenger);
+			if ( result ) {
+				res.statusCode = 200;
+				res.json('Challenger criado com sucesso!');
+			} else {
+				res.statusCode = 403;
+				res.json('Erro ao criar challenger');
+			}
 		} catch(err) {
 			res.statusCode = 400;
-			res.json('Erro ao crair challenger');
+			res.json('Erro ao criar challenger');
 		}
 	}
 
 });
 
 router.get('/', async (req, res) => {
+
+	const { year, day } = req.query;
+
 	try {
-		const challengers = await provider.findAll();
+		let challengers = [];
+		let query: any = '';
+		if (day) query = 'day';
+		if (year) query = 'year';
+
+		switch (query) {
+			case 'year':
+				challengers = await provider.findAllByYear(Number(year));
+				break;
+			case 'day':
+				challengers = await provider.findAllByDay(Number(day));
+				break;
+			default:
+				challengers = await provider.findAll();
+		}
+
 		res.statusCode = 200;
 		res.json(challengers);
 	} catch(err) {
 		res.statusCode = 400;
 		res.json('Erro ao buscar challengers');
+	}
+});
+
+router.get('/:id', async (req, res) => {
+	const id = req.params.id;
+	try {
+		const challenger = await provider.find(id);
+		res.statusCode = 200;
+		res.json(challenger);
+	} catch (err) {
+		res.statusCode = 400;
+		res.json('Erro ao pegar challenger!');
+	}
+})
+
+router.delete('/:id', async (req, res) => {
+	try {
+		const id = req.params.id;
+		await provider.delete(id);
+		res.statusCode = 200;
+		res.json('Challenger deletado com sucesso!');
+	} catch(err) {
+		res.statusCode = 500;
+		res.json('Erro ao deletar o Challenger');
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	const {name, day, year} = req.body;
+	const id = req.params.id;
+	try {
+		const challenger = new Challenger(id, name, day, year);
+		await provider.update(challenger);
+		res.statusCode = 200;
+		res.json('Sucesso ao atualizar o challenger!');
+	} catch (err) {
+		res.statusCode = 400;
+		res.json('Erro ao atualiar o challenger');
+	}
+});
+
+router.delete('/deadend/on', async (req, res) => {
+	try {
+		const result = await provider.deadend();
+		if (result) {
+			res.statusCode = 200;
+			res.json('DeadEnd iniciado!');
+		}
+		res.statusCode = 400;
+		res.json('Erro ao aplicar o deadend');
+	} catch (err) {
+		res.statusCode = 400;
+		res.json('Erro ao aplicar o deadend');
 	}
 });
 
--- src/providers/ChallengerProvider.ts
+++ src/providers/ChallengerProvider.ts
@@ -1,6 +1,5 @@
 import { getFirestore } from 'firebase-admin/firestore';
 import Challenger from '../model/Challenger';
-import { v4 as uuidv4 } from 'uuid';
 
 const database = getFirestore();
 
@@ -10,7 +9,35 @@ export default class ChallengerProvider {
 
 	async findAll() { // Pega todos os documentos da collection
 		try {
-			const snapshot = await database.collection(this.collection).get();
+			const snapshot = await database.collection(this.collection).orderBy('year', 'desc').get();
+			const challengers = [];
+			snapshot.forEach((doc) => {
+				challengers.push(doc.data());
+			});
+			return challengers;
+		} catch(err) {
+			console.log('Erro ao pegar challengers: ', err);
+		}
+		return [];
+	}
+
+	async findAllByYear(year: number) {
+		try {
+			const snapshot = await database.collection(this.collection).where('year', '==', year).get();
+			const challengers = [];
+			snapshot.forEach((doc) => {
+				challengers.push(doc.data());
+			});
+			return challengers;
+		} catch(err) {
+			console.log('Erro ao pegar challengers: ', err);
+		}
+		return [];
+	}
+
+	async findAllByDay(day: number) {
+		try {
+			const snapshot = await database.collection(this.collection).where('day', '==', day).get();
 			const challengers = [];
 			snapshot.forEach((doc) => {
 				challengers.push(doc.data());
@@ -25,7 +52,8 @@ export default class ChallengerProvider {
 	async find(id: string) { // Pega apenas um documento da collection
 		if (id) {
 			try {
-				return await database.collection(this.collection).doc(id).get();
+				const challenger = await database.collection(this.collection).doc(id).get();
+				return challenger.data();
 			} catch(err) {
 				console.log('Erro ao pegar challenger: ', err);
 			}
@@ -35,6 +63,15 @@ export default class ChallengerProvider {
 	async create(challenger: Challenger) { // Cria um novo documento na colleciton
 		if (challenger) {
 			try {
+				const challengers = await this.findAll();
+				challengers.forEach( (challengerA) => {
+					if (
+						challengerA.day == challenger.day &&
+						challengerA.year == challenger.year
+					) {
+						throw new Error('Já existe um challenger desse período.');
+					}
+				});
 				await database.collection(this.collection).doc(challenger.id).set(challenger.toJSON());
 				console.log('Challenger criado com sucesso!');
 				return true;
@@ -67,6 +104,20 @@ export default class ChallengerProvider {
 			} catch(err) {
 				console.log('Erro ao deletar challenger: ', err);
 			}
+		}
+		return false;
+	}
+
+	async deadend() {
+		try {
+			const challengers = await this.findAll();
+			challengers.forEach(async (challenger) => {
+				await this.delete(challenger.id);
+			});
+			console.log('DeadEnd aplicado. Todos os itens foram deletados!');
+			return true;
+		} catch (err) {
+			console.log('Erro ao aplicar o DeadEnd: ', err);
 		}
 		return false;
 	}
