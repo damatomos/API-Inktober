f581908ced4d84d01827cb477b0ce04f4368dfcd
BlueJayGM <damato578@gmail.com>
2021-12-29 17:29:53 -0300

    updated controller

--- src/controllers/ChallengerController.js
+++ src/controllers/ChallengerController.js
@@ -18,23 +18,32 @@ function validateItems(...items) {
 router.post('/', async (req, res) => {
     const { name, day, year } = req.body;
     const challenger = new Challenger_1.default({ name, day, year });
+    const result = await Challenger_1.default.where({ day, year }).findOne().exec();
+    if (result && result._id != null) {
+        res.statusCode = 403;
+        res.json({ msg: 'Já existe um Challenger nesse período.', data: result });
+        '';
+    }
     if (validateItems(name, day, year) && challenger) {
         try {
             const result = await challenger.save();
+            res.statusCode = 200;
             res.json({ msg: 'Sucesso ao criar o Challenger!', data: result });
         }
         catch (err) {
+            res.statusCode = 400;
             res.json({ msg: 'Erro ao criar o Challenger', data: err });
         }
     }
     else {
+        res.statusCode = 400;
         res.json({ msg: 'Itens inválidos, verifique os campos enviados!', data: null });
     }
 });
 router.get('/', async (req, res) => {
     const { year } = req.query;
     try {
-        let result = await Challenger_1.default.find(year ? { year } : {}).sort('day').exec();
+        let result = await Challenger_1.default.find(year ? { year } : {}).sort('year').sort('day').exec();
         let years = [];
         result.forEach((challenger) => {
             if (!years.includes(challenger.year)) {
@@ -55,14 +64,81 @@ router.get('/', async (req, res) => {
 });
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
+    try {
+        if (id) {
+            const result = await Challenger_1.default.findById(id);
+            if (result) {
+                res.statusCode = 200;
+                res.json({ msg: 'Sucesso ao encontrar o Challenger!', data: result });
+            }
+            else {
+                res.statusCode = 400;
+                res.json({ msg: 'Erro ao buscar Challengers!', data: null });
+            }
+        }
+        else {
+            res.statusCode = 400;
+            res.json({ msg: 'Identificado Inválido!', data: null });
+        }
+    }
+    catch (err) {
+        res.statusCode = 400;
+        res.json({ msg: 'Erro ao buscar Challengers!', data: err });
+    }
 });
 router.delete('/:id', async (req, res) => {
     const id = req.params.id;
+    try {
+        if (id) {
+            const result = await Challenger_1.default.findOneAndDelete({ _id: id }).exec();
+            if (result) {
+                res.statusCode = 200;
+                res.json({ msg: 'Sucesso ao deletar o Challenger!', data: result });
+            }
+            else {
+                res.statusCode = 400;
+                res.json({ msg: 'Erro ao deletar Challenger. Challenger não encontrado!', data: result });
+            }
+        }
+        else {
+            res.statusCode = 400;
+            res.json({ msg: 'Identificado Inválido!', data: null });
+        }
+    }
+    catch (err) {
+        res.statusCode = 400;
+        res.json({ msg: 'Erro ao deletar Challenger!', data: err });
+    }
 });
 router.put('/:id', async (req, res) => {
     const { name, day, year } = req.body;
     const id = req.params.id;
+    const challenger = await Challenger_1.default.findById(id);
+    if (validateItems(name, day, year) && id && challenger) {
+        try {
+            const result = await Challenger_1.default.where({ _id: id }).updateOne({ name, day, year });
+            res.statusCode = 200;
+            res.json({ msg: 'Sucesso ao editar o Challenger!', data: result });
+        }
+        catch (err) {
+            res.statusCode = 400;
+            res.json({ msg: 'Erro ao editar o Challenger', data: err });
+        }
+    }
+    else {
+        res.statusCode = 400;
+        res.json({ msg: 'Itens inválidos, verifique os campos enviados!', data: null });
+    }
 });
 router.delete('/deadend/on', async (req, res) => {
+    try {
+        const result = await Challenger_1.default.deleteMany({});
+        res.statusCode = 200;
+        res.json({ msg: 'DeadEnd iniciado!', data: result });
+    }
+    catch (err) {
+        res.statusCode = 400;
+        res.json({ msg: 'Erro ao iniciar DeadEnd', data: err });
+    }
 });
 exports.default = router;
--- src/controllers/ChallengerController.ts
+++ src/controllers/ChallengerController.ts
@@ -16,14 +16,24 @@ function validateItems(...items) {
 router.post('/', async (req, res) => {
 	const {name, day, year} = req.body;
 	const challenger = new Challenger({name, day, year});
+
+	const result = await Challenger.where({ day, year }).findOne().exec();
+	if ( result && result._id != null ) {
+		res.statusCode = 403;
+		res.json({msg: 'Já existe um Challenger nesse período.', data: result});''
+	}
+
 	if ( validateItems(name, day, year) && challenger ) {
 		try {
 			const result = await challenger.save();
+			res.statusCode = 200;
 			res.json({msg: 'Sucesso ao criar o Challenger!', data: result});
 		} catch (err) {
+			res.statusCode = 400;
 			res.json({msg: 'Erro ao criar o Challenger', data: err});
 		}
 	} else {
+		res.statusCode = 400;
 		res.json({msg: 'Itens inválidos, verifique os campos enviados!', data: null});
 	}
 
@@ -34,7 +44,7 @@ router.get('/', async (req, res) => {
 	const { year } = req.query;
 
 	try {
-		let result = await Challenger.find(year ? { year } : {}).sort('day').exec();
+		let result = await Challenger.find(year ? { year } : {}).sort('year').sort('day').exec();
 		let years = [];
 		result.forEach((challenger) => {
 			if (!years.includes(challenger.year)) {
@@ -55,18 +65,78 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
 	const id = req.params.id;
+	try {
+		if (id) {
+			const result = await Challenger.findById(id);
+			if (result){
+				res.statusCode = 200;
+				res.json({msg: 'Sucesso ao encontrar o Challenger!', data: result});
+			} else {
+				res.statusCode = 400;
+				res.json({msg: 'Erro ao buscar Challengers!', data: null});
+			}
+		} else {
+			res.statusCode = 400;
+			res.json({msg: 'Identificado Inválido!', data: null});
+		}
+	} catch (err) {
+		res.statusCode = 400;
+		res.json({msg: 'Erro ao buscar Challengers!', data: err});
+	}
 })
 
 router.delete('/:id', async (req, res) => {
 	const id = req.params.id;
+	try {
+		if (id){
+			const result = await Challenger.findOneAndDelete({_id: id}).exec();
+			if (result) {
+				res.statusCode = 200;
+				res.json({msg: 'Sucesso ao deletar o Challenger!', data: result});
+			} else {
+				res.statusCode = 400;
+				res.json({msg: 'Erro ao deletar Challenger. Challenger não encontrado!', data: result});
+			}
+		} else {
+			res.statusCode = 400;
+			res.json({msg: 'Identificado Inválido!', data: null});
+		}
+	} catch (err) {
+		res.statusCode = 400;
+		res.json({msg: 'Erro ao deletar Challenger!', data: err});
+	}
 });
 
 router.put('/:id', async (req, res) => {
 	const {name, day, year} = req.body;
 	const id = req.params.id;
+
+	const challenger = await Challenger.findById(id);
+
+	if ( validateItems(name, day, year) && id && challenger) {
+		try {
+			const result = await Challenger.where({_id: id}).updateOne({name, day, year});
+			res.statusCode = 200;
+			res.json({msg: 'Sucesso ao editar o Challenger!', data: result});
+		} catch (err) {
+			res.statusCode = 400;
+			res.json({msg: 'Erro ao editar o Challenger', data: err});
+		}
+	} else {
+		res.statusCode = 400;
+		res.json({msg: 'Itens inválidos, verifique os campos enviados!', data: null});
+	}
 });
 
 router.delete('/deadend/on', async (req, res) => {
+	try {
+		const result = await Challenger.deleteMany({});
+		res.statusCode = 200;
+		res.json({msg: 'DeadEnd iniciado!', data: result});
+	} catch(err) {
+		res.statusCode = 400;
+		res.json({msg: 'Erro ao iniciar DeadEnd', data: err});
+	}
 });
 
 export default router;
